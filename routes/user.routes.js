796b38d337e6b7e4562c7e45e96ac8557eba6926
vaibhavtiwari0112 <vaibhavtiwari0128@gmail.com>
2024-12-25 11:29:11 +0530

    added authentication for user routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const UserController = require("../controllers/user.controller");
+const authenticate = require("../middleware/authMiddleware.js");
 
 const router = express.Router();
 
-router.get("/:id", UserController.getUser);
-router.put("/:id", UserController.updateUser);
+router.get("/:id", authenticate, UserController.getUser);
+router.put("/:id", authenticate, UserController.updateUser);
 
 module.exports = router;
